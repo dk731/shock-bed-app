4625d1aae8f6a76e6195f0bb778c1d140ddc7416
Demid Kaidalov <demid.kaidalov@gmail.com>
2024-11-27 00:51:16 +0200

    Add ESLint and Tailwind CSS configuration, update dependencies, and enhance Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: [
+    "nuxt-quasar-ui",
+    "@nuxt/icon",
+    "@nuxt/eslint",
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+  ],
+  quasar: {
+    plugins: ["Dialog", "Loading", "Notify"],
+    extras: {
+      font: "roboto-font",
+      animations: "all",
+    },
+  },
+  app: {
+    head: {
+      title: "Shock Bed",
+    },
+  },
+  imports: {
+    dirs: ["./composables", "./utils", "./types"],
+  },
+  icon: {
+    serverBundle: {
+      collections: ["mdi", "material-symbols", "twemoji"],
+    },
+  },
+});
